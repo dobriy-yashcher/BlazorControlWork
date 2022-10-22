252f480fde02e93a1d0bccacff7e56646864e6de
dobriy-yashcher <golovinsergey17@gmail.com>
2022-10-22 08:38:03 +0300

    add RegistrationAuthorizationCore.

---
+++ RegistrationAuthorizationCore/User.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace RegistrationAuthorizationCore
+{
+    public class User
+    {
+        //[BsonDiscriminator("Unit")]
+        //public ObjectId _id;
+
+        public int IdUser { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+
+        public User() { }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
